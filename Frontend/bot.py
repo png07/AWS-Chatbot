62fa665d28f175e9e85470c5d6314e3a15f9cb2f
Pooja Nayak <poojanayak190303@gmail.com>
2025-02-14 19:57:44 +0530

    Update bot.py

--- Frontend/bot.py
+++ Frontend/bot.py
@@ -7,7 +7,7 @@ import os
 from datetime import datetime, timedelta
 
 # API Gateway URL
-API_GATEWAY_URL = API_Endpoint
+API_GATEWAY_URL = "https://79mo988gpl.execute-api.us-east-1.amazonaws.com/dev/chatbot"
 
 # Helper function to get user's unique ID (stored in session state)
 def get_user_id():
